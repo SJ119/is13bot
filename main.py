b422b706e1c146fdf02b6706849bb9b0a9a643ae
Shen Jin <sjin@Shens-Air.lan>
2016-04-05 22:51:34 -0400

    a

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def incoming():
 
             return Response(status=200)
 
-port = int(os.environ.get("PORT", 5000))
+port = int(os.environ.get("PORT", 33507))
 if __name__ == "__main__":
     app.run(port=port, debug=True)
 
